f0c9f133d38d99ff095190cb50069ae826baf0fa
LovableLavz <lavanya.december20@gmail.com>
2016-01-22 20:57:46 +0530

    Update medice_intake_update.php
    Assigning value intake and passing that value in query (line no:9)

--- mobileJson/medice_intake_update.php
+++ mobileJson/medice_intake_update.php
@@ -6,9 +6,10 @@
 	{
 		# write code to decode json
 		//$intake=json_decode($_REQUEST['intakemedicine'],true);
+		$intake = $_REQUEST['intake'];
 		$nikshay_no=$_REQUEST['Nikshay_no'];
 		#$query="UPDATE medicine_intake set medicine='1' where nikshay_no='$nikshay_no'";
-		$query="INSERT INTO  medicine_intake (Nikshay_no,intake) VALUES('$nikshay_no','True')";
+		$query="INSERT INTO  medicine_intake (Nikshay_no,intake) VALUES('$nikshay_no',$intake)";
 
 		$db= new Database();
 		$db->connect();
