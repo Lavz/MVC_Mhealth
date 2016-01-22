927575494a82b0792595b8f26d44b98b5f96d8f3
LovableLavz <lavanya.december20@gmail.com>
2016-01-22 23:12:53 +0530

    Sending Message to Dot provider Logic
     Checking Status and send message to dot provider.

--- mobileJson/medice_intake_update.php
+++ mobileJson/medice_intake_update.php
@@ -17,6 +17,28 @@
 				echo "1";
 		else
 				echo "0";
+				
+			// Checking Status and send message to dot provider.
+			// Sending Message to Dot provider Logic
+		        $user       =   'success';  //bhash username
+	    		$password   =   '123456';   //bhash password
+	    		$sender_id  =   'BSHSMS';   //approved senderid
+	    		//$reciever   =   '9629732192';
+	    		$name       = "Lavanya";
+	    		$sender     = '9629732192';
+	    		$email      = "Test Email";
+	    		$text       = "Test Message From mPower Application";
+	    		$priority   =   'ndnd';     // ndnd or dnd
+    			$sms_type   =   'normal';   //normal or flash message
+
+			$data = array('user'=>$user, 'pass'=>$password, 'sender'=>$sender_id, 'phone'=>$sender, 'text'=>$text, 'priority'=>$priority, 'stype'=>$sms_type);
+	       
+	        // Send the POST request with cURL
+	        $ch = curl_init('http://bhashsms.com/api/sendmsg.php?');
+	        curl_setopt($ch, CURLOPT_POST, true);
+	        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        $response = curl_exec($ch);	
 		$db->disconnect();
 	}
 ?>
